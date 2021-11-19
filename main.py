cac65dd64bcd62cce750883eeedf5919de1829d9
Ishaya Solomon <ishaya808@gmail.com>
2021-11-19 03:26:55 +0100

    files upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import random as r
+
+truths = [
+  'I love basketball',
+  'I can swim',
+  'I understand Javascript',
+  'I am a twin',
+  'I graduated at the top of my class in college',
+  'I grew up on an island',
+  'I have a sister who is ten years younger than I',
+  'I used to live in a treehouse',
+  'I was born in England',
+  'I was the first person in my family to go to the university',
+  'I was voted best student in math in high school',
+  'I wore the same jacket every day in high school',
+  'My dad is a secret agent (or other profession)',
+  'My sister is a vegetarian',
+  'I do not like Versace products',
+  'One of my cousins is a movie star (sports star)',
+  'When I was a kid, I wanted to be a movie star.',
+  'I hate spaghetti',
+  'I do not have a favourite musician',
+  'I am a christian'
+]
+
+lies = [
+  'I love chemistry',
+  'I love being broke',
+  'I made my first million during lockdown',
+  'I understand python',
+  'I am a fine girl',
+  'My mom is a nigerian',
+  'My favourite musician is Davido',
+  'I love studying',
+  'I have a friend named Jessica',
+  'I do not know how to approach girls',
+  'I can sing so well',
+  'I am a data scientist'
+]
+
