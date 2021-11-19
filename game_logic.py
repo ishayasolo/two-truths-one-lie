05a72a78116c38d4b6258f50f306244caa682c07
Ishaya Solomon <ishaya808@gmail.com>
2021-11-19 13:41:32 +0100

    second commit by author

---
+++ game_logic.py
@@ -0,0 +1,16 @@
+import random
+import statements as stm
+
+# launcher
+def init():
+  randomListOfTruths = random.sample(range(0, len(stm.truths) - 1), 2)
+  randomListOfLies = random.sample(range(0, len(stm.lies) - 1), 1)
+
+  # print 2 true statements
+  statements = [
+    stm.truths[randomListOfTruths[0]],
+    stm.truths[randomListOfTruths[1]],
+    stm.truths[randomListOfLies[0]]
+  ]
+
+  random.random()
---
+++ statements.py
@@ -0,0 +1,43 @@
+# true statements
+truths = [
+  'I love basketball',
+  'I can swim',
+  'I understand Javascript',
+  'I am a twin',
+  'I graduated at the top of my class in college',
+  'I grew up on an island',
+  'I have a sister who is ten years younger than I',
+  'I used to live in a treehouse',
+  'I was born in England',
+  'I was the first person in my family to go to the university',
+  'I was awarded best student in math in high school',
+  'I wore the same jacket every day in high school',
+  'My dad is a secret agent (or other profession)',
+  'My sister is a vegetarian',
+  'I do not like Versace products',
+  'One of my cousins is a movie star (sports star)',
+  'When I was a kid, I wanted to be a movie star.',
+  'I hate spaghetti',
+  'I do not have a favourite musician',
+  'I am a christian'
+]
+
+# a bunch of lies
+lies = [
+  'I love chemistry',
+  'I love being broke',
+  'I made my first million during lockdown',
+  'I understand python',
+  'I am a pretty girl',
+  'I was awarded the best student in Physics in higher school'
+  'My mom is a Nigerian',
+  'My favourite musician is Davido',
+  'I love studying',
+  'I have a friend named Jessica',
+  'I do not know how to approach girls',
+  'I can sing so well',
+  'I am a data scientist',
+  'I am a web developer',
+  'I always have tea for breakfast',
+  'I love midnight snacks'
+]
