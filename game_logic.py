6648ccd48913194e099e0df5df0bd6d6fedba888
Ishaya Solomon <ishaya808@gmail.com>
2021-11-22 17:04:36 +0100

    sixth commit by author

--- game_logic.py
+++ game_logic.py
@@ -3,21 +3,51 @@ import statements as stm
 
 # launcher
 def init():
-  # randomly print 2 true statements and one false statement
+  # create a random list containing two generated truths and one generated lie
   statements = [
-    stm.truths[random.sample(range(0, len(stm.lies)), 1)[0]],
-    stm.truths[random.sample(range(0, len(stm.lies)), 1)[1]],
-    stm.lies[random.sample(range(0, len(stm.lies)), 1)[0]]
+    stm.truths[random.randint(0, len(stm.truths) - 1)],
+    stm.truths[random.randint(0, len(stm.truths) - 1)],
+    stm.lies[random.randint(0, len(stm.lies) - 1)]
   ]
 
-  lie = statements[2]
+  shuffled_statements = []
 
-  shuffledIndices = random.sample(range(0, len(statements)), 3)
-  # print(statements[shuffledIndices[0]])
-  # print(statements[shuffledIndices[1]])
-  # print(statements[shuffledIndices[2]])
+  # generate a list of shuffled numbers between 0 and 2
+  random_list_of_indices = random.sample(range(0, 3), 3)
 
-  print(statements)
-  print(shuffledIndices)
+  print('\n*** statements ***')
+  
+  
+  x = 0
+  while x < 3:
+    shuffled_statements.append(statements[random_list_of_indices[x]])
+    print(statements[random_list_of_indices[x]])
+    
+    # if false statement is found store it in a variable called 'lie'
+    if statements[random_list_of_indices[x]] == statements[2]:
+      lie = statements[random_list_of_indices[x]]
+    
+    x += 1
 
-  guess = input('what\'s the lie?\n>>> ')
+  print('\nlie:', lie)
+
+  def validate():
+    if selection == lie:
+      print('correct!')
+    else:
+      print('wrong!')
+    
+  def marker():
+    global selection
+    if answer == '1':
+      selection = shuffled_statements[0]
+      validate()
+    elif answer == '2':
+      selection = shuffled_statements[1]
+      validate()
+    elif answer == '3':
+      selection = shuffled_statements[2]
+      validate()
+  
+  answer = input('\nwhich is the lie?\n>>> ')
+  marker()
