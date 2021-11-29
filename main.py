ebb08ece619aaba3758159e796bc51456efcb918
ishayasolo <ishaya808@gmail.com>
2021-11-29 21:30:16 +0100

    applied modularization to main.py and rules_of_the_game.py

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ purpose: csc318 - structured programming course work
 ****************************************************
 """
 
-import rules_of_the_game, game_logic
+import rules_of_the_game, game_logic, coin_merchant as cm
 
 # print(__doc__)
 
 # print the rules of the game
-rules_of_the_game.print_rules()
+rules_of_the_game.print_rules(cm)
 
 # launch game
 game_logic.init()
--- rules_of_the_game.py
+++ rules_of_the_game.py
@@ -1,4 +1,6 @@
-def print_rules():
+import coin_merchant as cm
+
+def print_rules(cm):
   print('\n*********************')
   print('* RULES OF THE GAME *')
   print('*********************')
@@ -7,7 +9,7 @@ def print_rules():
   print('- two (2) out of my three (3) statements are true')
   print('- one is a BIG FAT lie')
   print('- your job is to spot the lie')
-  print('- you begin with 50 coins')
+  print('- you begin with', cm.get_coins(), 'coins')
   print('- you guess correctly, you earn 10 coins')
   print('- you guess wrongly, you lose 10 coins')
   print('- type \'ctrl + shift + c\' at any point to force-close the program')
